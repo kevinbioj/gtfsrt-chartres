eaa903d75d14915a46d8ac50b18b893dfc03a75d
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-02-14 21:01:24 +0100

    fix coordinates

--- src/utils/extract-coordinates.ts
+++ src/utils/extract-coordinates.ts
@@ -1,4 +1,12 @@
+import proj4 from "proj4";
+
+proj4.defs(
+	"EPSG:2154",
+	"+proj=lcc +lat_0=46.5 +lon_0=3 +lat_1=49 +lat_2=44 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs",
+);
+
 export const extractCoordinates = (input: string) => {
-	const [lon, lat] = input.split(" ");
-	return { latitude: +lat, longitude: +lon };
+	const [x, y] = input.split(" ");
+	const [lon, lat] = proj4("EPSG:2154", "WGS84", [+x, +y]);
+	return { latitude: lat, longitude: lon };
 };
